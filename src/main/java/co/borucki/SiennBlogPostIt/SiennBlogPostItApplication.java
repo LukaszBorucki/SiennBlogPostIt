238e3a593c9e7d33974585d2ca0a4bb0c3d034d6
LukaszBorucki <lukasz@borucki.co>
2018-02-20 01:04:01 +0100

    Add SiennBlogPostItApplication

---
+++ src/main/java/co/borucki/SiennBlogPostIt/SiennBlogPostItApplication.java
@@ -0,0 +1,12 @@
+package co.borucki.SiennBlogPostIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiennBlogPostItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiennBlogPostItApplication.class, args);
+    }
+}

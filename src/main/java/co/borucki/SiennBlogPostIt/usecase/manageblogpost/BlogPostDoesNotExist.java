b822b1022ee56aaac770f29717b06db4845bc861
LukaszBorucki <lukasz@borucki.co>
2018-02-21 19:31:14 +0100

    Updated BlogPostDoesNotExist
    Added @ResponseStatus annotation

--- src/main/java/co/borucki/SiennBlogPostIt/usecase/manageblogpost/BlogPostDoesNotExist.java
+++ src/main/java/co/borucki/SiennBlogPostIt/usecase/manageblogpost/BlogPostDoesNotExist.java
@@ -1,5 +1,9 @@
 package co.borucki.SiennBlogPostIt.usecase.manageblogpost;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BlogPostDoesNotExist extends RuntimeException {
     public BlogPostDoesNotExist(long blogPostId) {
         super("Post with id: " + blogPostId + " does not exist");

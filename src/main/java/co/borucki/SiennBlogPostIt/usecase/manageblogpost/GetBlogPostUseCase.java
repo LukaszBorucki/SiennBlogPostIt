e8f94506c0680e2b5d29ab8ed9432a13f5ad3daa
LukaszBorucki <lukasz@borucki.co>
2018-02-21 19:39:08 +0100

    Updated GetBlogPostUseCase
    Added getHeaders() method

--- src/main/java/co/borucki/SiennBlogPostIt/usecase/manageblogpost/GetBlogPostUseCase.java
+++ src/main/java/co/borucki/SiennBlogPostIt/usecase/manageblogpost/GetBlogPostUseCase.java
@@ -1,6 +1,8 @@
 package co.borucki.SiennBlogPostIt.usecase.manageblogpost;
 
 import co.borucki.SiennBlogPostIt.domain.BlogPost;
+import co.borucki.SiennBlogPostIt.domain.BlogPostHeader;
+import co.borucki.SiennBlogPostIt.entrypoint.blogpost.BlogPostHeaderFactory;
 import co.borucki.SiennBlogPostIt.repositories.BlogPostRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +12,11 @@ import java.util.stream.Collectors;
 @Service
 public class GetBlogPostUseCase {
     private final BlogPostRepository blogPostRepository;
+    private final BlogPostHeaderFactory blogPostHeaderFactory;
 
-    public GetBlogPostUseCase(BlogPostRepository blogPostRepository) {
+    public GetBlogPostUseCase(BlogPostRepository blogPostRepository, BlogPostHeaderFactory blogPostHeaderFactory) {
         this.blogPostRepository = blogPostRepository;
+        this.blogPostHeaderFactory = blogPostHeaderFactory;
     }
 
     public List<BlogPost> getAll() {
@@ -43,5 +47,9 @@ public class GetBlogPostUseCase {
         return getAll().
                 stream().
                 collect(Collectors.groupingBy(w -> w.getPostCategory().getName()));
+    }
+
+    public List<BlogPostHeader> getHeaders() {
+        return blogPostHeaderFactory.createFromBlogPost(getAll());
     }
 }

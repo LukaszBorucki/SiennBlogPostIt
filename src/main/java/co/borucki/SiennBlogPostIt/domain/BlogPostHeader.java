dc61b796a0456c9d7d8bf5a8382184a0eb8a734e
LukaszBorucki <lukasz@borucki.co>
2018-02-21 18:13:56 +0100

    Create BlogPostHeader object

---
+++ src/main/java/co/borucki/SiennBlogPostIt/domain/BlogPostHeader.java
@@ -0,0 +1,31 @@
+package co.borucki.SiennBlogPostIt.domain;
+
+public class BlogPostHeader {
+    private long id;
+    private String title;
+    private String contents;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}

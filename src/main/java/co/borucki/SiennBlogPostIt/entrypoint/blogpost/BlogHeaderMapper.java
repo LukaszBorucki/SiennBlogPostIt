9a051f2675b6c0a7d5838ef5245560de8ff7b4b5
LukaszBorucki <lukasz@borucki.co>
2018-02-21 19:44:56 +0100

    Updated BlogHeaderMapper
    Added getHeaders() method

--- src/main/java/co/borucki/SiennBlogPostIt/entrypoint/blogpost/BlogHeaderMapper.java
+++ src/main/java/co/borucki/SiennBlogPostIt/entrypoint/blogpost/BlogHeaderMapper.java
@@ -9,7 +9,11 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public class BlogHeaderMapper {
     public static BlogHeaderDTO toHeaderDTO(BlogPost blogPost) {
-        return new BlogHeaderDTO(blogPost.getId(), blogPost.getTitle(), blogPost.getContents());
+        BlogHeaderDTO blogHeaderDTO = new BlogHeaderDTO();
+        blogHeaderDTO.setContents(blogPost.getContents());
+        blogHeaderDTO.setTitle(blogPost.getTitle());
+        blogHeaderDTO.setId(blogPost.getId());
+        return blogHeaderDTO;
     }
 
     public static List<BlogHeaderDTO> toHeaderDTO(List<BlogPost> blogPosts) {

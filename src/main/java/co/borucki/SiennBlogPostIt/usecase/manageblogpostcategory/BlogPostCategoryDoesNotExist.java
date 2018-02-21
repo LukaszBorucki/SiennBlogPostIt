a2d46e29ec1e8b7a424db51e735681ec51b7810e
LukaszBorucki <lukasz@borucki.co>
2018-02-21 19:34:24 +0100

    Updated BlogPostCategoryDoesNotExist
    Added @ResponseStatus annotation

--- src/main/java/co/borucki/SiennBlogPostIt/usecase/manageblogpostcategory/BlogPostCategoryDoesNotExist.java
+++ src/main/java/co/borucki/SiennBlogPostIt/usecase/manageblogpostcategory/BlogPostCategoryDoesNotExist.java
@@ -1,5 +1,9 @@
 package co.borucki.SiennBlogPostIt.usecase.manageblogpostcategory;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BlogPostCategoryDoesNotExist extends RuntimeException {
     public BlogPostCategoryDoesNotExist(long id) {
         super("Post category with id: " + id + " does not exist");

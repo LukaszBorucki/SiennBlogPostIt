b2d2bf48a8b5bae2dc2d6ebc22914245132c1db0
LukaszBorucki <lukasz@borucki.co>
2018-02-21 19:26:35 +0100

    Updated BlogAuthorDoesNotExist
    Add @ResponseStatus annotation

--- src/main/java/co/borucki/SiennBlogPostIt/usecase/manageblogauthor/BlogAuthorDoesNotExist.java
+++ src/main/java/co/borucki/SiennBlogPostIt/usecase/manageblogauthor/BlogAuthorDoesNotExist.java
@@ -1,5 +1,9 @@
 package co.borucki.SiennBlogPostIt.usecase.manageblogauthor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BlogAuthorDoesNotExist extends RuntimeException {
     public BlogAuthorDoesNotExist(long blogAuthorId) {
         super("Author with id: " + blogAuthorId + " does not exist");
